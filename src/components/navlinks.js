3cfdb08d2a9f1236b1fc2837de0fb983196856aa
David Torres <info@davidtorres.design>
2021-01-09 22:23:18 -0800

    Update Nav Links to point to real URLs, seperate data and markup in component

--- src/components/navlinks.js
+++ src/components/navlinks.js
@@ -1,19 +1,32 @@
+const socialLinks = [
+	{
+		url: "http://www.linkedin.com/in/davidtorresdesign",
+		img: "/img/linkedin.svg",
+		title: "LinkedIn - David Torres Design",
+	},
+	{
+		url: "https://github.com/D-T-Design",
+		img: "/img/github.svg",
+		title: "GitHub - David Torres Design",
+	},
+	{
+		url: "https://stackoverflow.com/users/11743348/davidtorresdesign",
+		img: "/img/stackoverflow.svg",
+		title: "Stack Overflow - David Torres Design",
+	},
+];
 function NavLinks() {
 	return (
 		<div className="links">
 			<button>
-				Download Resume <img src="/img/pdf.svg" alt="Click Here" />
+				Download Resume <img src="/img/pdf.svg" alt="Click Here to Download my Resume" />
 			</button>
 			<div className="social">
-				<button href="#">
-					<img src="/img/linkedin.svg" alt="" />
-				</button>
-				<button href="#">
-					<img src="/img/github.svg" alt="" />
-				</button>
-				<button href="#">
-					<img src="/img/stackoverflow.svg" alt="" />
-				</button>
+				{socialLinks.map((social, index) => (
+					<a key={index} href={social.url} target="_blank" rel="noreferrer">
+						<img src={social.img} alt={social.title} />
+					</a>
+				))}
 			</div>
 		</div>
 	);
